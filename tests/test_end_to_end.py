05378bf9fb8af03ee8395fb5e3b8cf0486bb2132
Artemshuc <www.umbo@gmail.com>
2025-02-03 00:43:19 +0300

    внедрение allure

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
-import time
+import allure
 
 @pytest.fixture(scope="module")
 def driver():
@@ -15,22 +15,22 @@ def driver():
 @pytest.fixture(scope="module")
 def logged_in_driver(driver):
     driver.get('https://www.saucedemo.com/')
-    
+
     login = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, 'user-name'))
     )
     login.send_keys('standard_user')
-    
+
     password = driver.find_element(By.ID, 'password')
     password.send_keys('secret_sauce')
-    
+
     button = driver.find_element(By.ID, 'login-button')
     button.click()
-    
+
     WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, 'inventory_item'))
     )
-    
+
     return driver
 
 @pytest.fixture(scope="function")
@@ -49,28 +49,37 @@ def failed_login_driver(driver):
 
     return driver
 
+@allure.epic('SauceDemo Tests')
+@allure.feature('Login Feature')
 def test_login(logged_in_driver):
+    """Тест для проверки успешного входа в систему."""
     assert logged_in_driver.current_url.endswith("inventory.html")
 
+@allure.feature('Cart Feature')
 def test_add_to_card(logged_in_driver):
+    """Тест для проверки добавления товара в корзину."""
     add_to_card = WebDriverWait(logged_in_driver, 10).until(
         EC.presence_of_element_located((By.ID, 'add-to-cart-sauce-labs-backpack'))
     )
     add_to_card.click()
-    
+
     cart_badge = logged_in_driver.find_element(By.CLASS_NAME, 'shopping_cart_badge')
     assert cart_badge.text == "1", "Товар не добавлен в корзину"
 
+@allure.feature('Cart Feature')
 def test_remove_from_card(logged_in_driver):
+    """Тест для проверки удаления товара из корзины."""
     remove_button = WebDriverWait(logged_in_driver, 10).until(
         EC.presence_of_element_located((By.ID, 'remove-sauce-labs-backpack'))
     )
     remove_button.click()
-    
+
     cart_badge = logged_in_driver.find_elements(By.CLASS_NAME, 'shopping_cart_badge')
     assert len(cart_badge) == 0, "Товар не удалён из корзины"
 
+@allure.feature('Product Sorting')
 def test_price_filter_low_to_high(logged_in_driver):
+    """Тест для проверки сортировки товаров по цене от низкой к высокой."""
     filter_dropdown = WebDriverWait(logged_in_driver, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, 'product_sort_container'))
     )
@@ -89,15 +98,16 @@ def test_price_filter_low_to_high(logged_in_driver):
 
     assert price_list == sorted(price_list), "Товары не отсортированы по возрастанию цены"
 
+@allure.feature('Product Sorting')
 def test_price_filter_high_to_low(logged_in_driver):
-    filter_high = WebDriverWait(logged_in_driver,10).until(
+    """Тест для проверки сортировки товаров по цене от высокой к низкой."""
+    filter_high = WebDriverWait(logged_in_driver, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, "product_sort_container"))
     )
     Select(filter_high).select_by_visible_text("Price (high to low)")
 
-
-    prices = WebDriverWait(logged_in_driver,10).until(
-        EC.presence_of_all_elements_located ((By.CLASS_NAME, "inventory_item_price"))
+    prices = WebDriverWait(logged_in_driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, "inventory_item_price"))
     )
     price_list = []
 
@@ -106,17 +116,14 @@ def test_price_filter_high_to_low(logged_in_driver):
         price_list.append(float(price_text))
 
     assert price_list == sorted(price_list, reverse=True)
-    
+
+@allure.feature('Login Feature')
 def test_failed_login(failed_login_driver):
+    """Тест для проверки неверного входа в систему."""
     error_message = WebDriverWait(failed_login_driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, 'h3[data-test="error"]'))
     )
     assert "Epic sadface: Username and password do not match any user in this service" in error_message.text, "Неверное сообщение об ошибке"
-
-
-
-
-
 
 
 
